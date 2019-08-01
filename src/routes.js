4cc9b9a7733507bf2f9077c3b3806c849516040e
diberry <diberry@microsoft.com>
2019-08-01 05:45:18 -0700

    move hardcoded text of html to index.html file

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@
 const multer = require('multer')
 const inMemoryStorage = multer.memoryStorage();
 const singleFileUpload = multer({ storage: inMemoryStorage });
+const path = require('path');
 
 const azureStorage = require('./azureStorage.js');
 
 const setUpRoutes = (app) => {
+    
     app.all('*', function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -16,30 +18,24 @@ const setUpRoutes = (app) => {
             next();
         }
     });
-    app.get('/', (req, res, next) => {
-        res.send(
-        '<form action="/upload" method="post" enctype="multipart/form-data">' +
-        '<input type="file" name="uploadedFile" /><br>' +
-        '<input type="text" name="container" value="myContainer" /><br>' + 
-        '<input type="text" name="directory" value="myDirectory" /><br>' +
-        '<input type="submit" value="Upload" /><br>' +
-        '</form>'
-        );
-    });
 
     app.post('/upload', singleFileUpload.single('uploadedFile'), azureStorage.uploadFile);
 
-    app.use(function (err, req, res, next) {
-
-        if (res.headersSent) {
-            return next(err)
-          }
-          res.status(500)
-          res.render('error', { error: err })
+    app.get('/', function (req, res, next) {
+        res.sendFile(path.join(__dirname, '../client/index.html'));
     });
 
-}
+    app.use(function (err, req, res, next) {
 
+        console.log(`error handler ${err}`);
+
+        if (res.headersSent) {
+            return next(err);
+          }
+          res.status(500).send(err);
+
+    });
+}
 
 module.exports = {
     setUpRoutes:setUpRoutes
